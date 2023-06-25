3f78119836290fa7465a374a8ba2c8482a1ab9d0
Kyle Hadland <kylehadland@gmail.com>
2023-06-26 06:46:35 +1000

    github pages setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
 
-import react from "@astrojs/react";
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://kylehadland.com.au",
+  // base: "/kh-web-astro",
   experimental: {
-    assets: true
+    assets: true,
   },
-  integrations: [tailwind(), react()]
-});
+  integrations: [tailwind(), react()],
+})
